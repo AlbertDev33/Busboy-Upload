4a1b7c15dbf4467df6cae8ab0242a3ce9bd1fda3
AlbertDev33 <alber725@gmail.com>
2021-01-26 15:49:00 -0300

    Implementação do Repository para criar a url no banco

---
+++ src/database/mongodb/typeorm/repositories/UrlUploadsRepository.ts
@@ -0,0 +1,25 @@
+import { MongoRepository, getMongoRepository } from 'typeorm';
+
+import IUrlUploadsRepository from '../../../repositories/IUrlUploadsRepository';
+
+import UrlUploads from '../schemas/UrlUploads';
+
+class UrlUploadsRepository implements IUrlUploadsRepository {
+  private ormRepository: MongoRepository<UrlUploads>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(UrlUploads, 'mongo');
+  }
+
+  public async create(url: string): Promise<UrlUploads> {
+    const urlUploads = this.ormRepository.create({
+      url,
+    });
+
+    await this.ormRepository.save(urlUploads);
+
+    return urlUploads;
+  }
+}
+
+export default UrlUploadsRepository;

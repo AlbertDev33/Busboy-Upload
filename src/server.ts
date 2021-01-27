ca92fe33832aca04479363274c52cac92b8928cd
AlbertDev33 <alber725@gmail.com>
2021-01-27 15:28:14 -0300

    Implementação de uma rota estática para exibir as imagens

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use('/files', express.static(uploadConfig.uploadFolder));
 app.use(routes);
 
 app.listen(3333, () => {

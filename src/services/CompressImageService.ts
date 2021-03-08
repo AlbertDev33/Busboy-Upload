c44ed97ff955ba0611b7f80f869c120baf3f2caa
AlbertDev33 <alber725@gmail.com>
2021-03-08 10:45:27 -0300

    refactor: change variable name and add filename on database

--- src/services/CompressImageService.ts
+++ src/services/CompressImageService.ts
@@ -5,7 +5,7 @@ import uploadConfig from '../config/upload';
 import * as StorageCloudProvider from '../providers/StorageProvider/implementations/StorageCloudProvider';
 import * as StorageLocalProvider from '../providers/StorageProvider/implementations/DiskStorageProvider';
 import * as CompressImage from '../providers/CompressImageProvider/implementation/ResizedProvider';
-import * as UrlUploadsRepository from '../database/mongodb/typeorm/repositories/UrlUploadsRepository';
+import * as UrlUploadsRepository from '../infra/database/mongodb/typeorm/repositories/UrlUploadsRepository';
 
 export class CompressImageService {
   constructor(
@@ -29,32 +29,35 @@ export class CompressImageService {
     const [https, , url] = urlUploads.split('/');
     const newUrlPath = `${https}//${url}`;
 
-    const sizes = [128, 48, 32, 24, 16];
+    const fileSizes = [128, 48, 32, 24, 16];
 
     const chooseStorageProvider =
       uploadConfig.driver === 's3'
         ? this.storageCloudProvider
         : this.storageLocalProvider;
 
-    sizes.forEach(async size => {
-      const newFile = `${fileHashName}-${size}.${extension}`;
+    fileSizes.forEach(async fileSize => {
+      const newFile = `${fileHashName}-${fileSize}.${extension}`;
       const fileParams = `${tempFolder}/${newFile}`;
 
       await this.compressImage.generateCompressImage({
         filePath,
-        size,
+        size: fileSize,
         newFile: fileParams,
       });
 
       if (uploadConfig.driver === 's3') {
-        await this.urlUploadsRepository.create(`${newUrlPath}/${newFile}`);
+        await this.urlUploadsRepository.create(
+          `${newUrlPath}/${newFile}`,
+          newFile,
+        );
       }
 
       await chooseStorageProvider.saveFile(newFile);
     });
 
     if (uploadConfig.driver === 's3') {
-      await this.urlUploadsRepository.create(urlUploads);
+      await this.urlUploadsRepository.create(urlUploads, fileHashName);
     }
     await chooseStorageProvider.saveFile(fileName);
 

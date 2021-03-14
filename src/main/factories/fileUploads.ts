2fd1351f148c0f07f356c31e2c533c29d2e9591e
AlbertDev33 <alber725@gmail.com>
2021-03-14 14:29:31 -0300

    feat: class implementations

--- src/main/factories/fileUploads.ts
+++ src/main/factories/fileUploads.ts
@@ -1,10 +1,32 @@
+import { UrlUploadsRepository } from '../../infra/database/mongodb/typeorm/repositories/UrlUploadsRepository';
+import { LocalStorageProvider } from '../../providers/StorageProvider/implementations/LocalStorageProvider';
+import { CloudStorageProvider } from '../../providers/StorageProvider/implementations/CloudStorageProvider';
+import { ResizedProvider } from '../../providers/CompressImageProvider/implementation/ResizedProvider';
+import { FilePathProvider } from '../../providers/FilePathProvider/implementation/FilePathProvider';
+
 import { UploadBusboyController } from '../../controller/UploadBusboyController';
 import { CompressImageService } from '../../services/CompressImageService';
 import { HashFileProvider } from '../../providers/RandomFileProvider/HashFileProvider';
 
 export const makeUploadController = (): UploadBusboyController => {
-  const compressImageService = new CompressImageService();
+  const urlUploadsRepository = new UrlUploadsRepository();
+  const localStorageProvider = new LocalStorageProvider();
+  const cloudStorageProvider = new CloudStorageProvider();
+  const resizedProvider = new ResizedProvider();
+  const filePathProvider = new FilePathProvider();
+
+  const compressImageService = new CompressImageService(
+    urlUploadsRepository,
+    localStorageProvider,
+    cloudStorageProvider,
+    resizedProvider,
+    filePathProvider,
+  );
   const hashFileProvider = new HashFileProvider();
 
-  return new UploadBusboyController(compressImageService, hashFileProvider);
+  return new UploadBusboyController(
+    compressImageService,
+    hashFileProvider,
+    filePathProvider,
+  );
 };

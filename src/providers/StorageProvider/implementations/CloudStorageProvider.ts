8c19220e822ef01e8a81a0e583e8a35ea74a00ed
AlbertDev33 <alber725@gmail.com>
2021-03-14 14:26:11 -0300

    refactor: rename file

---
+++ src/providers/StorageProvider/implementations/CloudStorageProvider.ts
@@ -0,0 +1,42 @@
+import aws from 'aws-sdk';
+import path from 'path';
+import fs, { createReadStream } from 'fs';
+import mime from 'mime';
+
+import { ICloudStorageProvider } from '../models/ICloudStorageProvider';
+import uploadConfig from '../../../config/upload';
+
+export class CloudStorageProvider implements ICloudStorageProvider {
+  constructor(
+    private client = new aws.S3({
+      region: 'us-east-1',
+    }),
+  ) {}
+
+  public async saveFile(file: string): Promise<string> {
+    const uploadPath = path.resolve(uploadConfig.tmpFolder, file);
+
+    const contentType = mime.getType(uploadPath);
+
+    if (!contentType) {
+      throw new Error('File not found');
+    }
+
+    const contentFile = createReadStream(uploadPath);
+
+    await this.client
+      .putObject({
+        Bucket: uploadConfig.config.aws.bucket,
+        Key: file,
+        ContentType: contentType,
+        ACL: 'public-read',
+        Body: contentFile,
+        ContentDisposition: `inline; filename-${file}`,
+      })
+      .promise();
+
+    await fs.promises.unlink(uploadPath);
+
+    return file;
+  }
+}
